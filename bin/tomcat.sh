53175ea9b9ed5706b5422ef69255b713c90a977c
Neil Ellis <neil@cazcade.com>
2015-02-02 05:28:09 +0000

    Initial work

--- bin/tomcat.sh
+++ bin/tomcat.sh
@@ -3,4 +3,6 @@ if [ ! -f /.tomcat_admin_created ]; then
     /app/bin/create_tomcat_user.sh
 fi
 
+envsubst '$ADMIN_EMAIL:$ADMIN_PASSWORD:$USERGRID_URL:$MAIL_USER:$MAIL_PASSWORD:$MAIL_HOST:$MAIL_PORT' < /app/etc/usergrid.properties > /tomcat/lib/usergrid.properties
+
 /sbin/setuser app /tomcat/bin/catalina.sh run
